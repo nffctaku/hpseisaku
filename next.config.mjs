e2f06d54cc0634c808c3894cc8f2f62e791d344a
nffctaku <footballtopjp96@gmail.com>
2025-10-28 19:43:45 +0900

    build: disable eslint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   serverExternalPackages: ['firebase-admin'],
   images: {
     remotePatterns: [
