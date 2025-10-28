b5a76440057964d1d28237f8a4640da64d9467ac
nffctaku <footballtopjp96@gmail.com>
2025-10-28 21:08:34 +0900

    fix: correctly handle private key newlines

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -16,7 +16,7 @@ try {
     const serviceAccount = {
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     };
 
     admin.initializeApp({
